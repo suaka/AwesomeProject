3bd0262302408b059e0dafc85f0066e59e25126b
jyh <suaka1280@naver.com>
2024-01-06 00:07:11 +0900

    practics

---
+++ components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greeting() {
+  return (
+    <View>
+      <Text>이건실시간으로 반영될수도있는지??? 함수 컴포넌트!!</Text>
+    </View>
+  );
+}
+
+export default Greeting;
