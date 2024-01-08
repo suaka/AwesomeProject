e5d4a230ed1690b7e86ba5bd01cc5f27e481cfba
jyh <suaka1280@naver.com>
2024-01-09 00:23:06 +0900

    프롭에 대한설명

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-function Greeting() {
+function Greeting(props) {
   return (
     <View>
-      <Text>이건실시간으로 반영될수도있는지??? 함수 컴포넌트!!</Text>
+      <Text>안녕하세요{props.name}!</Text>
     </View>
   );
 }
+
+Greeting.defaultProps = {
+  name: '리액트 네이티브',
+};
 
 export default Greeting;
