e4c43e9b924a4b7c34aa739f7040e91c9d057023
jyh <suaka1280@naver.com>
2024-01-09 00:24:02 +0900

    업데이트

--- components/Greeting.js
+++ components/Greeting.js
@@ -3,9 +3,12 @@ import {View, Text} from 'react-native';
 
 function Greeting(props) {
   return (
-    <View>
-      <Text>안녕하세요{props.name}!</Text>
-    </View>
+    <>
+      <View>
+        <Text>안녕하세요{props.name}!</Text>
+      </View>
+      <Text>안녕하세요_빈태그이용{props.name}!</Text>
+    </>
   );
 }
 
